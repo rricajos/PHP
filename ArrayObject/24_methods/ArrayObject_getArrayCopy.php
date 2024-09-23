466d19cdbe876fb50a91a57ee245aa66990164d5
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-09-23 21:35:36 +0200

    getFlags

--- ArrayObject/24_methods/ArrayObject_getArrayCopy.php
+++ ArrayObject/24_methods/ArrayObject_getArrayCopy.php
@@ -1,4 +1,36 @@
 <?php
+// ArrayObject_getArrayCopy.php
 
-// This file is for the method: getArrayCopy
+// Create an initial associative array
+$array = [
+    'name' => 'Alice',
+    'age' => 30,
+    'city' => 'Wonderland'
+];
 
+// Create an instance of ArrayObject using the constructor
+$arrayObject = new ArrayObject($array);
+
+// Output the initial state of the ArrayObject
+echo "Initial ArrayObject:\n";
+print_r($arrayObject);
+
+// Get a copy of the underlying array
+$arrayCopy = $arrayObject->getArrayCopy();
+
+// Output the copied array
+echo "\nCopied Array:\n";
+print_r($arrayCopy);
+
+// Modify the original ArrayObject
+$arrayObject->name = 'Bob';
+$arrayObject['country'] = 'Wonderland';
+
+// Output the modified ArrayObject
+echo "\nModified ArrayObject:\n";
+print_r($arrayObject);
+
+// Output the copied array again to show it remains unchanged
+echo "\nCopied Array After Modification:\n";
+print_r($arrayCopy);
+?>
--- ArrayObject/24_methods/ArrayObject_getFlags.php
+++ ArrayObject/24_methods/ArrayObject_getFlags.php
@@ -1,4 +1,26 @@
 <?php
 
-// This file is for the method: getFlags
+// ArrayObject_getFlags.php
 
+// Setting the intern array and flags to getFlags()
+$array_as_props = new ArrayObject(['name' => 'ricky'], ArrayObject::ARRAY_AS_PROPS);
+$std_prop_list = new ArrayObject(['name' => 'rocky'], ArrayObject::STD_PROP_LIST);
+
+// Setting new props bouth
+$array_as_props->age = 23;
+$std_prop_list->age = 23;
+
+// The key of question - Where are the props?
+echo "ARRAY_AS_PROPS flag - ArrayObject Properties:\n";
+print_r(get_object_vars($array_as_props)); 
+
+echo "\n\nARRAY_AS_PROPS flag - ArrayObject Intern Array:\n";
+print_r($array_as_props->getArrayCopy()); 
+
+echo "\n\nSTD_PROP_LIST flag - ArrayObject Properties:\n";
+print_r(get_object_vars($std_prop_list)); 
+
+echo "\n\nSTD_PROP_LIST flag - ArrayObject Intern Array:\n";
+print_r($std_prop_list->getArrayCopy()); 
+
+?>
